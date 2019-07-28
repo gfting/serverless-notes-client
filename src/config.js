332fa25f9b92a6c623075689f812f4af469dab91
Gabriel Ting <gabrielfting@gmail.com>
2019-07-27 22:48:57 -0400

    first commit

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "serverless-notes-uploads-bucket"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://tt2uuq0bw5.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_Feid0poEA",
+    APP_CLIENT_ID: "7f1f9jofamcuhrvhenrcjc7gnp",
+    IDENTITY_POOL_ID: "us-east-1:e3366e24-0af5-472a-9ac9-ac825fab13f6"
+  }
+};
